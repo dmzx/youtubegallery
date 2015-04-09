451d83f468101e004e7dd54afb84a931141a90e7
dmzx <info@dmzx-web.net>
2015-04-09 07:09:52 +0200

    code update
    code update

--- ext.php
+++ ext.php
@@ -4,7 +4,7 @@
 * @package phpBB Extension - Youtube Videos Gallery
 * @copyright (c) 2015 dmzx - http://www.dmzx-web.net
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* @Author Galandas - http://phpbb3world.com
+* @Author _Vinny_ - http://www.suportephpbb.com.br
 *
 */
 
